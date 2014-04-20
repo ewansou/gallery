539127e508796253f45a3009694a119ff517e25a
ewansou <ewansou@hotmail.com>
2014-04-20 22:42:10 +0800

    changed hash to nba

--- model/config.js
+++ model/config.js
@@ -30,7 +30,7 @@ config = {
 		instagram: {
 			// change number of image showed when the page first load
 			number_of_image: 30,
-			tagName: 'tcpartistes',
+			tagName: 'nba',
 			unsubscribe_tag_id: '4651662'
 		}
 };
