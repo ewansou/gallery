ac0cf8bd941859af1058f238c16b57951921e65b
ewansou <ewansou@hotmail.com>
2014-04-30 00:10:14 +0800

    changed hash

--- model/config.js
+++ model/config.js
@@ -29,8 +29,8 @@ config = {
 		imgfoldername: "/imageupload/",
 		instagram: {
 			// change number of image showed when the page first load
-			number_of_image: 15,
-			tagName: 'tcpartistes'
+			number_of_image: 100,
+			tagName: 'christieturns24'
 		}
 };
 exports.config = config;
