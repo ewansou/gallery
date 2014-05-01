fbc199861ba1778e70d9bfc2d432f9d0d9c74096
ewansou <ewansou@hotmail.com>
2014-05-01 23:15:33 +0800

    update package.json

--- server.js
+++ server.js
@@ -109,15 +109,16 @@ Instagram.subscriptions.subscribe({
 */
 // if you want to unsubscribe to any hashtag you subscribe
 // just need to pass the ID Instagram send as response to you
-Instagram.subscriptions.unsubscribe({ id: '4745441' });
-Instagram.subscriptions.unsubscribe({ id: '4747693' });
-Instagram.subscriptions.unsubscribe({ id: '4747734' });
+//Instagram.subscriptions.unsubscribe({ id: '4745441' });
+//Instagram.subscriptions.unsubscribe({ id: '4747693' });
+//Instagram.subscriptions.unsubscribe({ id: '4747734' });
 //Instagram.subscriptions.unsubscribe({ id: config.instagram.unsubscribe_tag_id }); //this is for #nba
 //Instagram.subscriptions.unsubscribe({ id: '4610657' }); this is for #picobunny
-/**
+
 Instagram.tags.unsubscribe_all({
   callback_url: 'http://instagram-real-time.herokuapp.com/callback'
-});*/
+});
+
 // https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
 io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
